ca083fb82b377d15e96a9348e4dbf706e1fe9b2c
sieunhann <manhcuong200997@gmail.com>
2022-04-01 14:25:02 +0700

    update 04/01/22 ver1

---
+++ lesson_3/homework/src/vn/techmaster/Main.java
@@ -0,0 +1,42 @@
+package vn.techmaster;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Câu 1: ");
+        String string = "Hello every one";
+        int count = 0;
+        System.out.println("Kí tự e xuất hiện tại vị trí: ");
+        for (int i = 0; i < string.length(); i++){
+            if(string.charAt(i) == 'e'){
+                count++;
+                System.out.println("Index " + count + ": "+ i);
+            }
+        }
+        System.out.println("Số lần kí tự e xuất hiện là: " + count);
+
+        System.out.println("\nCâu 2: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập chuỗi: ");
+        String palin = sc.nextLine();
+
+        boolean flag = true;
+
+        if(palin.length() % 2 == 0){
+            for(int i = 0; i < palin.length()/2; i++){
+                if(palin.charAt(i) != palin.charAt(palin.length()-(i+1))){
+                    flag = false;
+                }
+            }
+        } else {
+            flag = false;
+        }
+        if(flag == true){
+            System.out.println("Chuỗi palindrome");
+        } else {
+            System.out.println("Không phải chuỗi palindrome");
+        }
+    }
+}
